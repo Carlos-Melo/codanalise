89046ff6a03c02064c7a26afe7815e93a19856c0
Carlos-Melo <carloszdu@gmail.com>
2021-11-09 19:45:09 -0300

    fix bugs

--- src/app/modules/usuario/shared/usuario.service.ts
+++ src/app/modules/usuario/shared/usuario.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from './usuario';
